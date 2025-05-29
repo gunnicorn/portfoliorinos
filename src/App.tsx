e86d08792884c474944ff3ece986ace6134a026e
gunnicorn <anggunq@hotmail.com>
2025-05-29 14:25:43 +0800

    fixed not used error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Anchoring from "./components/ContentCards";
 import styles from "./App.module.css";
-import SelfIntroCard from "./components/Cards/SelfIntroCard";
+// import SelfIntroCard from "./components/Cards/SelfIntroCard";
 import HandmadeCard from "./components/Cards/HandmadeCard";
 
 function App() {
