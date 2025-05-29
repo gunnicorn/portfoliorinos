9a00133ec90f6e2aff9f301cde0b6f9dcf680233
gunnicorn <anggunq@hotmail.com>
2025-05-29 14:46:42 +0800

    adjusted main padding and added react icons

--- src/components/Cards/HandmadeCard.tsx
+++ src/components/Cards/HandmadeCard.tsx
@@ -1,26 +1,30 @@
 import {
-    LinkedinOutlined,
-    GithubOutlined,
-    MailOutlined
-  } from '@ant-design/icons';
-import React from 'react';
+  LinkedinOutlined,
+  GithubOutlined,
+  MailOutlined,
+} from "@ant-design/icons";
+import React from "react";
 import styles from "./HandmadeCard.module.css";
+import { FaMapMarkerAlt } from "react-icons/fa";
+
 const HandmadeCard: React.FC = () => (
   <div className={styles.handmadeMain}>
     <div className={styles.handmadeMain1}>
-        <div className={styles.handmadeTitle}>
-            Anggun Qistina
-        </div>
-        <div className={styles.handmadeDesc}>
-            GAME DEVELOPER | SOFTWARE ENGINEER
-        </div>
+      <div className={styles.handmadeTitle}>Anggun Qistina</div>
+      <div className={styles.handmadeDesc}>
+        GAME DEVELOPER | SOFTWARE ENGINEER
+      </div>
+      <div>
+        <FaMapMarkerAlt style={{ color: "black", fontSize: "30px" }} />
+        Singapore
+      </div>
     </div>
     <div className={styles.handmadeMain2}>
-        <div className={styles.handmadeContact}>
-        <LinkedinOutlined style={{fontSize: 35}}/>
-        <GithubOutlined style={{fontSize: 35}}/>
-        <MailOutlined style={{fontSize:35}}/>
-        </div>
+      <div className={styles.handmadeContact}>
+        <LinkedinOutlined style={{ fontSize: 35 }} />
+        <GithubOutlined style={{ fontSize: 35 }} />
+        <MailOutlined style={{ fontSize: 35 }} />
+      </div>
     </div>
   </div>
 );
